c63e369d0109f428e3f329f3d9632f929343f415
a.chmylenko <achmylenko58@gmail.com>
2024-07-02 14:38:53 +0300

    InitiateMigration class

---
+++ code/API/Utils/InitiateMigration.php
@@ -0,0 +1,31 @@
+<?php
+namespace API\Utils;
+use API\Migrate\Zendesk;
+use API\Retreive\Freshdesk;
+
+class InitiateMigration {
+    function startMigration(): void
+    {
+        $env = parse_ini_file('.env');
+        $freshdeskCredentials = [
+            'apikey' => base64_encode($env['FRESHDESK_TOKEN']),
+            'url' => $env['FRESHDESK_URL']
+        ];
+
+        $zendeskCredentials = [
+            'apikey' => 'Basic ' . base64_encode($env['ZENDESK_EMAIL'] . '/token:' . $env['ZENDESK_TOKEN']),
+            'url' => $env['ZENDESK_URL']
+        ];
+
+        $zendesk = new Zendesk($zendeskCredentials);
+
+        $migration = new Freshdesk($freshdeskCredentials);
+
+        $zendesk->importTicket($migration->getTickets());
+
+    }
+}
+
+
+
+
